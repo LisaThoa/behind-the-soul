86a13b36332b4edc395a56fdabbfcc8e50d2fe85
Lisa Heslot <74857590+LisaThoa@users.noreply.github.com>
2026-01-19 22:40:10 +0100

    wip

---
+++ js/autoForm.js
@@ -0,0 +1,57 @@
+document.addEventListener("submit", (e) => {
+  if (e.target.classList.contains("auto-form")) {
+    e.preventDefault();
+    fillFields(e.target);
+  }
+});
+
+function fillFields(form) {
+  let targetContainer = document.getElementById(
+    form.dataset.resultId,
+  )?.children;
+  let targets = [];
+  for (const target of targetContainer) {
+    targets[target.id] = target;
+  }
+
+  let elements = form.children;
+  for (let element of elements) {
+    if (element.nodeName === "LABEL") {
+      let children = element.children;
+      for (const child of children) {
+        if (child.nodeName === "INPUT") element = child;
+      }
+    }
+    if (
+      (element.nodeName === "INPUT" || element.nodeName === "TEXTAREA") &&
+      element.type !== "submit"
+    ) {
+      let targetField = targets[element.name];
+      fillField(targetField, element);
+    }
+  }
+  let htmlCode = document.getElementById(form.dataset.resultId);
+  navigator.clipboard.writeText(htmlCode.outerHTML);
+}
+
+function fillField(targetField, element) {
+  if (element.type === "checkbox") {
+    if (element.checked) {
+      fillDefault(targetField, element.value);
+    } else {
+      fillDefault(targetField, "");
+    }
+    return;
+  }
+  if (element.type === "radio") {
+    if (element.checked) {
+      fillDefault(targetField, element.value);
+    }
+    return;
+  }
+  fillDefault(targetField, element.value);
+}
+
+function fillDefault(targetField, value) {
+  targetField.replaceChildren(value);
+}
